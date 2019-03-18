703a7d6565419479900961ba433a2eed63a444f9
orathai <student06@Student06.local>
2019-03-18 14:28:15 +0700

    Start Data Type

---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปรนะจ้ะ
+var myName = "orathai"
+myName = "Doramon"
+
+//Explicit Constant
+let myFriend = "Nopita"
+//myName = "Sunako"Error Because myFriend is let 
+
+
+
+
+
